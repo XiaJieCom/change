3feb6271ef1b1d16bac8bfee9d8154dd75644686
XiaJie <xiajieok@gmail.com>
2015-08-06 17:10:08 +0800

    qsbk

--- python/py_win/download_first.py
+++ python/py_win/download_first.py
@@ -1,28 +1,21 @@
-#第一篇
+#1、打印第一篇文章链接
 import urllib
-
 url = ['']*40
-
 i = 0
-
-con = urllib.urlopen('http://blog.sina.com.cn/s/articlelist_1191258123_0_1.html').read()
-
+url = 'http://blog.sina.com.cn/s/articlelist_1191258123_0_1.html'
+con = urllib.urlopen(url).read()
 title = con.find(r'<a title=')
-
 href = con.find(r'href=',title)
-
 html = con.find(r'.html',href)
-
 url = con[href +6 :html +5 ]
-
 print url
 
-#第一页
-
+#2.1打印第一页所有文章链接
 import urllib
 url = ['']*40
 i = 0
-con = urllib.urlopen('http://blog.sina.com.cn/s/articlelist_1191258123_0_1.html').read()
+url = 'http://blog.sina.com.cn/s/articlelist_1191258123_0_1.html'
+con = urllib.urlopen(url).read()
 title = con.find(r'<a title=')
 href = con.find(r'href=',title)
 html = con.find(r'.html',href)
@@ -37,12 +30,13 @@ while title != -1 and href != -1 and html != -1 and i < 40:
     html = con.find(r'.html',href)
     i = i +1
 	
-#改编
+#2.2改编
 
 import urllib
 
 i = 0
-con = urllib.urlopen('http://blog.sina.com.cn/s/articlelist_1191258123_0_1.html').read()
+url = 'http://blog.sina.com.cn/s/articlelist_1191258123_0_1.html'
+con = urllib.urlopen(url).read()
 title = con.find(r'<a title=')
 href = con.find(r'href=',title)
 html = con.find(r'.html',href)
@@ -60,35 +54,9 @@ while i < 50:
     i = i + 1
 	
 	
-#zhihu 
-import urllib
-
-i = 0
-url = ['']*40
-con = urllib.urlopen('http://www.zhihu.com/collection/19668036').read()
-target = con.find(r'<a target="_blank')
-base = con.find(r'href=',target)
-end = con.find('>',base)
-url[0] = 'http://www.zhihu.com' + con[target +25 :end - 1]
-print url[0]
 
 
-while i < 20:
-  url[0] = 'http://www.zhihu.com' + con[target +25 :end - 1]
-  print url[0]
-  target = con.find(r'<a target="_blank',end)
-  base = con.find(r'href=',target)
-  end = con.find('>',base)
-  i = i + 1
-while j < 30:
-    content = urllib.urlopen(url[j]).read()
-    print url[0]
-    open(r'zhihu/'+url[j],'w+').write(content)
-    print 'downloading',
-    j = j + 1
-    time.sleep(15)  
-
-#zhihu_download
+#3、下载知乎某一页的所有链接
 #!/usr/bin/env python
 import time
 import urllib
@@ -97,13 +65,11 @@ i = 0
 j = 0
 url = ['']*30
 name = ['']*30
-con = urllib.urlopen('http://www.zhihu.com/collection/19668036').read()
+html = 'http://www.zhihu.com/collection/19668036'
+con = urllib.urlopen(html).read()
 target = con.find(r'<a target="_blank')
 base = con.find(r'href=',target)
 end = con.find('>',base)
-url[0] = 'http://www.zhihu.com' + con[target +25 :end - 1]
-#print url[0]
-
 
 while target != -1 and base != -1 and end != -1 and i < 30:
   url[0] = 'http://www.zhihu.com' + con[target +25 :end - 1]
@@ -112,7 +78,44 @@ while target != -1 and base != -1 and end != -1 and i < 30:
   base = con.find(r'href=',target)
   end = con.find('>',base)
   content = urllib.urlopen(url[0]).read()
-  open(r'zhihu/'+name[0]+'.html','w+').write(content)
-  print 'downloading',name[0]
-  time.sleep(5)
+  open(r'D:/Document/Linux/python/tmp/'+name[0]+'.html','w+').write(content)
+  print 'Downloading ...',name[0]
+  time.sleep(3)
   i = i + 1
+
+
+#4、下载所有页上的所有链接
+
+import time
+import urllib
+
+page = 1
+url = ['']*350
+i = 0
+link = 1
+while page <= 7:
+  con = urllib.urlopen('http://blog.sina.com.cn/s/articlelist_1191258123_0_'+str(page)+'.html').read()
+  title = con.find(r'<a title=')
+  href = con.find(r'href=',title)
+  html = con.find(r'.html',href)
+  while title != -1 and href != -1 and html != -1 and i < 350:
+    url[i] = con[href +6 :html +5 ]
+    print link,url[i]
+    title = con.find(r'<a title=',html)
+    href = con.find(r'href=',title)
+    html = con.find(r'.html',href)
+    link = link + 1
+    i = i +1
+  else:
+    print 'find end!'
+  page = page + 1
+else:
+    print 'all find end'
+j = 0
+while j < 50:
+    content = urllib.urlopen(url[j]).read()
+    open(r'tmp/'+url[j][-26:],'w+').write(content)
+    j = j + 1
+    time.sleep(5)
+else:
+    print 'download over!'
--- python/py_win/qsbk.py
+++ python/py_win/qsbk.py
@@ -4,13 +4,14 @@ import random
 import urllib2
 from bs4 import BeautifulSoup
 
-p = 1
 url = 'http://www.qiushibaike.com/text/page/'
 
 my_headers = [
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0',
     'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)',
+    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
     'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0',
+    'Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0',
     'ELinks/0.12pre5 (textmode; Linux; -)'
 ]
 
@@ -38,18 +39,17 @@ def get_txt(haha):
         con = cont[head + 16:end]
         print str(i), con
         i = i + 1
-#        time.sleep(3)
+        time.sleep(3)
 page = raw_input("Please input a number:")
 p = int(page)
 while p < 36:
-	haha = get_con(url + str(p) + '?s=4796159', my_headers)
-	print get_txt(haha)
-	print "这是第" + str(p) + "页"
-	p = p + 1
-	
+  haha = get_con(url + str(p) + '?s=4796159', my_headers)
+  print get_txt(haha)
+  print "这是第" + str(p) + "页"
+  p = p + 1
+  
 '''
 <a href="/history/page/2?s=JMJJ9J6SGXDILL0T">2</a>
 http://www.qiushibaike.com/history/page/3?s=JMJJ9J6SGXDILL0T
-
 250
 '''
