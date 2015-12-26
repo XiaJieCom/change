741ab8ebf74c78b4c0e3aaaed780f1c899abcc03
jack <jack@jackdeMacBook-Pro.local>
2015-12-26 16:26:50 +0800

    `$1`

--- Demo/s1/ts1.py
+++ Demo/s1/ts1.py
@@ -1,2 +1,49 @@
+#!/usr/bin/env python3
 #__author__ = 'jack'
+'''
 print("hahah")
+'''
+'''
+luckey_nm = 6
+
+while True:
+    choice = int(input("Please input you want :"))
+    if choice == luckey_nm:
+        print("Bingo!!!")
+        break
+    elif choice > luckey_nm:
+        print("Too big!!!")
+    else:
+        print("Too small!!!")
+
+'''
+
+'''
+luckey_nm = 6
+choice = 0
+while luckey_nm != choice:
+    choice = int(input("Please input you want :"))
+    #if choice == luckey_nm:
+    #    print("Bingo!!!")
+    #    break
+    if choice > luckey_nm:
+        print("Too big!!!")
+    elif choice < luckey_nm:
+        print("Too small!!!")
+print("Bingo")
+'''
+luckey_nm = 6
+choice = 0
+i = 0
+while i < 3:
+    choice = int(input("Please input you want :"))
+    if choice > luckey_nm:
+        print("Too bigger...")
+    elif choice < luckey_nm:
+        print("Too smaller...")
+    else:
+        print("Bingo!")
+        break
+    i += 1
+else:
+    print("Too many request...")
