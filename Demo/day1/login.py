377865f975e7c63c4e8fa3eae501e0ed59d90084
jack <jack@jackdeMacBook-Pro.local>
2015-12-30 06:27:23 +0800

    login.py

--- Demo/day1/login.py
+++ Demo/day1/login.py
@@ -45,30 +45,62 @@ else:
     print("Too many times to try again...")
 
 """
+
+
+'''
+#定义函数，负责输入账号和密码
+while True:
+    choice = input("Please input your choice, regstry or login (r/l/q)? ")
+    if choice == 'r':
+        #如果选择注册，就写入passd.txt文件
+        input_info()
+        print("Your name is %s, and passwd is %s."%(i_name,i_passwd))
+        with open('passwd.txt','a+') as f:
+            f.write(i_name + ':' + i_passwd + '\n' )
+    elif choice == 'l':
+        #否则就是登录，判断用户名密码是否匹配，3次退出
+        i = 0
+        while i < 3:
+            input_info()
+            f = open('passwd.txt').read()
+            if f.find(i_name +':'+ i_passwd) !=-1:
+                print("welcome my world!")
+                exit()
+            else:
+                print("Invalid user...")
+                i += 1
+        else:
+            print("Too many times to try again...")
+            print("You must stop, %s!"% i_name)
+            with open('stop.txt','a+') as f:
+                f.write(i_name + '\n' )
+                time.sleep(10)
+    else:
+        print("You can go out!!!")
+        exit()
+
+'''
+
 import time
 def input_info():
     global i_name
     i_name = input("Please input your name: ").strip()
     global i_passwd
     i_passwd = input("Please input your passwd: ").strip()
-
-#定义函数，负责输入账号和密码
-choice = input("Please input your choice, regstry or login ? ")
-if choice == 'registry':
-    #如果选择注册，就写入passd.txt文件
+def registry():
     input_info()
     print("Your name is %s, and passwd is %s."%(i_name,i_passwd))
     with open('passwd.txt','a+') as f:
         f.write(i_name + ':' + i_passwd + '\n' )
-elif choice == 'login':
-    #否则就是登录，判断用户名密码是否匹配，3次退出
+def login():
     i = 0
     while i < 3:
         input_info()
         f = open('passwd.txt').read()
-        if f.find(i_name +':'+ i_passwd) !=-1:
+        stop = open('stop.txt').read()
+        if f.find(i_name +':'+ i_passwd) !=-1 and stop.find(i_name) == -1:
             print("welcome my world!")
-            break
+            exit()
         else:
             print("Invalid user...")
             i += 1
@@ -77,7 +109,17 @@ elif choice == 'login':
         print("You must stop, %s!"% i_name)
         with open('stop.txt','a+') as f:
             f.write(i_name + '\n' )
-            time.sleep(10)
-else:
-    print("You can go out!!!")
+            print("You have been added to the black list.\n3 seconds after the withdrawal system!")
+            time.sleep(3)
+            exit()
 
+
+while True:
+    choice = input("Please input your choice, regstry or login (r/l/q)? ")
+    if choice == 'r':
+        registry()
+    elif choice == 'l':
+        login()
+    else:
+        print("You can go out!!!")
+        exit()
